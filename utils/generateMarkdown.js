1f42d2dc7b9bdec4c6e10ce0aa39da116fe0815e
EYUP <jobmoonday2@gmail.com>
2024-01-12 18:51:37 +0000

    generateMarkdown.js file has been prepared

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,54 +1,50 @@
-function generateMarkdown(data) {
-    return `# ${data.title} (${data.license})
-  
-  ## Description
-  
-  ${data.description}
-  
-  
-  ## Table Of Content
-  
-  - [Description](#description)
-  - [Table Of Contents](#table-of-content)
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributors)
-  - [Questions](#questions)
-  
+function generateMarkdown (data) {
+   
+    return ` #${data.title} (${data.license})
     
-  ## Installation
-  
-  ${data.inst}
-  
-  
-  ## Usage
-  
-  ${data.usage}
-  
-  
-  ## License
-  
-  ${data.license}
-  
-  
-  ## Contributors
-  
-  ${data.contrb}
-  
-  
-  ## Tests
-  
-  ${data.tests}
-  
-  
-  ## Questions
-  
-  Any question please contact:
+    ## Description
+    
+    ${data.description}
+
+    ## Table of Contents
+
+    - [Description] (#description)
+    - [Table Of Contents] (#table-of-content)
+    - [Installation] (#installation)
+    - [Usage] (#usage)
+    - [License] (#license)
+    - [Contributors] (#contributors)
+    - [Tests] (#)
+    - [Questions] (#questions)
+
+
+    ## Installation
+
+    ${data.inst}
+
+    ##Usage
+
+    ${data.usage}
+
+    ##License
+
+    ${data.license}
+
+    ##Contributors
+
+    ${data.contrb}
+
+    ##Tests
+
+    ${data.tests}
+
+    ##Questions
+
+    Any question please contact:
   - https://github.com/${data.github}
   - ${data.email}
-  
-  `;
-  }
-  
+      
+    `;
+
+}
   module.exports = generateMarkdown;
